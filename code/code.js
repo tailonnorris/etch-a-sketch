02aa73453edf6a92d410194cc9ece609883befc2
tyler <thailer2000@gmail.com>
2024-06-17 17:53:05 -0400

    create board algorithm & board styling

--- code/code.js
+++ code/code.js
@@ -1,6 +1,18 @@
 let board = document.querySelector(".board");
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  createBoard(16);
+  createBoard(32);
 });
 
+function createBoard(size){
+  for (let i = 0; i < size; i++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 1; j <= size ; j++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      column.appendChild(row);
+    };
+    board.appendChild(column);
+  };
+};
